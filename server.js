25db0c82c8391a2487190311da9f99f016961b00
Cristian Cartofeanu <cristian.cartofeanu@gmail.com>
2016-03-02 11:56:45 +0200

    Connecting to the database
    https://mlab.com/

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect('mongodb://root:qwerty123@ds019048.mlab.com:19048/e-commerce-course-project', function(err){
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("Connected to the database")
+	}
+})
 
 // Middleware
 app.use(morgan('dev'));
