0e001479b386f5439ecfd1b0d86befdf40b9b30b
Cristian Cartofeanu <cristian.cartofeanu@gmail.com>
2016-03-02 11:49:27 +0200

    Set up UserSchema for database
    Set up UserSchema for database along with setting up hashing algorithm
    for saving the password

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express');
+var morgan = require('morgan');
 
 var app = express();
+
+// Middleware
+app.use(morgan('dev'));
 
 app.get('/', function (req, res) {
 	var name = "Cristi"
 	res.json("My name is " + name)
 }) 
+
+
 
 app.listen(3000, function(err) {
 	if (err) throw err;
