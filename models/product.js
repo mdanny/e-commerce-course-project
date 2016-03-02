b20ddf36d7efe0ec264b89858db11789167dcdac
Cristian Cartofeanu <cristian.cartofeanu@gmail.com>
2016-03-02 20:16:35 +0200

    Adding Category/Product Schema

---
+++ models/product.js
@@ -0,0 +1,11 @@
+var monogoose = reqiore('mongoose');
+var Schema = mongoose.Schema;
+
+var ProductSchema = new Schema({
+	category: { true: Schema.Types.ObjectId, ref: 'Category'},
+	name: String,
+	price: Number,
+	image: String
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
