832401bb73c310ae8778f0d5435d1f591f3e51d3
Daniel Macrinici <macrinici.d@gmail.com>
2016-03-04 20:29:22 +0200

    added cookie, session and flash middleware

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
-var ejs_mate = require('ejs-mate');
+var engine = require('ejs-mate');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('express-flash');
 
 var User = require('./models/user');
 
 var app = express();
 
-mongoose.connect('mongodb://root:qwerty123@ds019048.mlab.com:19048/e-commerce-course-project', function(err){
+mongoose.connect('mongodb://root:qwerty123@ds019468.mlab.com:19468/ecommerce', function(err){
 	if (err) {
 		console.log(err);
 	} else {
@@ -20,18 +23,27 @@ mongoose.connect('mongodb://root:qwerty123@ds019048.mlab.com:19048/e-commerce-co
 // Middleware
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.engine('ejs', ejs_mate);
+app.use(cookieParser());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "dan!@#$"
+}));
+app.use(flash()); //flash is dependent on session and cookie
+
+
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
 var mainRoutes = require('./routes/main');
 var userRoutes = require('./routes/user');
 
 app.use(mainRoutes);
-app.use(userRoutes);  
+app.use(userRoutes);
 
-app.listen(3000, function(err) {
+app.listen(1234, function(err) {
 	if (err) throw err;
 	console.log("Server is Running");
 })
