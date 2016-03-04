7bdc777733955b56f2350a778912b25420310128
Daniel Macrinici <macrinici.d@gmail.com>
2016-03-04 20:28:37 +0200

    modified main and user route, to support flash errors

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
-var router = require('express').Router(); 
+var router = require('express').Router();
 var User = require('../models/user');
 
 
 router.get('/signup', function(req, res, next){
-	res.render('accounts/signup')
+	res.render('accounts/signup', {
+		errors: req.flash('errors')
+	});
 });
 
 router.post('/signup', function(req, res, next){
@@ -15,14 +17,14 @@ router.post('/signup', function(req, res, next){
 
 	User.findOne({ email: req.body.email }, function(err, existingUser){
 		if(existingUser) {
-			console.log(req.body.email + "is already exist");
+			req.flash('errors', 'Account with that email address already exists');
 			return res.redirect('/signup');
 		} else {
 			user.save(function(err, user){
 				if (err) return next(err);
+				return res.redirect('/')
 
-				res.json("New user has been created");
-			})
+			});
 		}
 	});
 });
