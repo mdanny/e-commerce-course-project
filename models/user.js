db4fbaa948769cec80890d28bcbed8ec8949e4ff
Daniel Macrinici <macrinici.d@gmail.com>
2016-03-05 01:05:11 +0200

    added custom gravatar method

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
-var Schema = mongoose.Schema
+var Schema = mongoose.Schema;
+var crypto = require('crypto');
 
 
 /* The user schema attributes / characteristics / fields */
@@ -24,7 +25,7 @@ var UserSchema = new mongoose.Schema({
 
 /* Hash the password before we even save it to the database */
 UserSchema.pre('save', function(next){
-	var user = this; 
+	var user = this;
 	if (!user.isModified('password')) return next();
 	bcrypt.genSalt(10, function(err, salt){
 		if (err) return next(err);
@@ -40,6 +41,13 @@ UserSchema.pre('save', function(next){
 /* compare password in the database and the one that the user type in */
 UserSchema.methods.comparePassword = function(password){
 	return bcrypt.compareSync(password, this.password);
-};
+}
+
+UserSchema.methods.gravatar = function(size) {
+  if (!this.size) size = 200;
+  if (!this.email) return 'https://gravatar.com/avatar/?s' + size + '&d=retro';
+  var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  return 'https://gravatar.com/avatar/' + md5 + '?s=' + size + '&d=retro';
+}
 
 module.exports = mongoose.model('User', UserSchema);
